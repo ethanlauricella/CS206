334b905c229bd72676eee9e5a267c9fe5188e263
ethanlauricella <93352713+ethanlauricella@users.noreply.github.com>
2022-01-19 10:47:56 -0500

    Box moving!

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_URDF("box.URdf")
 
 pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
 
--- simulation.py
+++ simulation.py
@@ -1,12 +1,16 @@
 import pybullet as p
 import time as t
+import pybullet_data
+
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+planeId = p.loadURDF("plane.urdf")
 
 physicsClient = p.connect(p.GUI)
-
 p.loadSDF("box.sdf")
-for i in range(1000):
-    p.stepSimulation
-    t.sleep(1/60)
-    print(i)
+
+for i in range(10000):
+    p.stepSimulation()
+    t.sleep(1./240.)
+    #print(i)
 
 p.disconnect()
