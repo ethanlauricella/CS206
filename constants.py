529324e599703797ea7828d7c352218a0f26eda5
ethanlauricella <93352713+ethanlauricella@users.noreply.github.com>
2022-04-18 14:46:16 -0400

    Update to compare with the hidden neuron bot

--- constants.py
+++ constants.py
@@ -1,26 +1,23 @@
 import math as math
 import numpy as np
 
-iterations = 4000
-grav = -9.8
+iterations = 1000
+grav = -20.
 
 windows = 0
 linux = 1
 comp = 0
 
-numberOfGenerations = 2
-populationSize = 2
+numberOfGenerations = 20
+populationSize = 5
 
-motorJointRange = 1.
+motorJointRange = 1.5
 
 numSensorNeurons = 13
 numMotorNeurons = 8
 
-force = 100
+force = 150
 # sin function CPP
-freq = 200
+freq = 100
 
-# Generate.py, in Generate_Brain()
-sensors = np.arange(numSensorNeurons)
-motors = np.arange(numSensorNeurons,numMotorNeurons)
 
